442a9878ce12ae0a8ccfd8b9bc85254ecb8b59aa
libo <libo@libo.(none)>
2011-08-18 15:42:03 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+
+var server = require('./lib/server');
+
+server.start();
---
+++ test.js
@@ -0,0 +1,7 @@
+
+var http = require('http');
+
+http.createServer(function(req,res){
+  res.writeHead(200,{'Content-Type':'text/plain'});
+  res.end('hello');
+}).listen(8080);
